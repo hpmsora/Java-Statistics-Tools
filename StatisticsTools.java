d609afa1807e40d887dbf96fbc10edb8c94a737a
Won Yong Ha <hpmsora@gmail.com>
2017-03-01 20:58:57 -0500

    1 March 2017

--- StatisticsTools.java
+++ StatisticsTools.java
@@ -13,6 +13,14 @@ class StatisticsTools {
 	System.out.println("Avg 1 : " + average2DArray(x, 0) + "2 : " + average2DArray(x, 1) + "3: " + average2DArray(x,2));
 
 	System.out.println("Std 1 : " + standardDiv2DArray(x, 0) + "2 : " + standardDiv2DArray(x, 1) + "3: " + standardDiv2DArray(x,2));
+
+	double[] xCI1 = confidenceInterval2DArray(x, 1.96, 0);
+	double[] xCI2 = confidenceInterval2DArray(x, 1.96, 1);
+	double[] xCI3 = confidenceInterval2DArray(x, 1.96, 2);
+	
+	System.out.println("CI 1 : [ " + xCI1[0] + " , " + xCI1[1] + " ]");
+	System.out.println("CI 2 : [ " + xCI2[0] + " , " + xCI2[1] + " ]");
+	System.out.println("CI 3 : [ " + xCI3[0] + " , " + xCI3[1] + " ]");
 	
 	double[] y = new double[100];
 
@@ -22,29 +30,11 @@ class StatisticsTools {
 
 	System.out.println("Avg: " + average1DArray(y));
 	
-	System.out.println("Std: " + standardDiv1DArray(y));	
-    }
+	System.out.println("Std: " + standardDiv1DArray(y));
 
-    public static double average1DArray(double[] x) {
-	double sum = 0;
-	int arrayLength = x.length;
+	double[] yCI = confidenceInterval1DArray(y, 1.96);
 
-	for(int i = 0; i < arrayLength; i++) {
-	    sum += x[i];
-	}
-
-	return sum / arrayLength;
-    }
-    
-    public static double average2DArray(double[][] x, int columnOrder) {
-	double sum = 0;
-	int arrayLength = x.length;
-
-	for (int i = 0; i < arrayLength; i++) {
-	    sum += x[i][columnOrder];
-	}
-	
-	return sum / arrayLength;
+	System.out.println("[ " + yCI[0] + " , " + yCI[1] + " ]");
     }
 
     public static double[] generateRandom1DArray(int numberOfRows, double range1, double range2) {
@@ -69,28 +59,74 @@ class StatisticsTools {
 	return array;
     }
 
-    public static double standardDiv1DArray(double[] x) {
-	double sum = 0, avg;
+    public static double average1DArray(double[] x) {
+	double sum = 0;
+	int arrayLength = x.length;
 
-	avg = average1DArray(x);
+	for(int i = 0; i < arrayLength; i++) {
+	    sum += x[i];
+	}
+
+	return sum / arrayLength;
+    }
+    
+    public static double average2DArray(double[][] x, int columnOrder) {
+	double sum = 0;
+	int arrayLength = x.length;
+
+	for (int i = 0; i < arrayLength; i++) {
+	    sum += x[i][columnOrder];
+	}
 	
-	for (int i = 0; i < x.length; i++) {
+	return sum / arrayLength;
+    }
+
+    public static double standardDiv1DArray(double[] x) {
+	double sum = 0;
+	double avg = average1DArray(x);
+	int size = x.length;
+	
+	for (int i = 0; i < size; i++) {
 	    sum += Math.pow(avg - x[i], 2);
 	}
 
-	return Math.sqrt(sum);
+	return Math.sqrt(sum / size);
     }
 
     public static double standardDiv2DArray(double[][] x, int columnOrder) {
-	double sum = 0, avg;
+	double sum = 0;
+	double avg = average2DArray(x, columnOrder);
+	int size = x.length;
 	
-	avg = average2DArray(x, columnOrder);
-
-	for (int i = 0; i < x.length; i++) {
+	for (int i = 0; i < size; i++) {
 	    sum += Math.pow(avg - x[i][columnOrder], 2);
 	}
 
-	return Math.sqrt(sum);
+	return Math.sqrt(sum / size);
+    }
+
+    public static double[] confidenceInterval1DArray(double[] x, double zScore) {
+	double avg = average1DArray(x);
+	double std = standardDiv1DArray(x);
+	double meanOfError = zScore * std / Math.sqrt(x.length);
+	double[] result = new double[2];
+
+	result[0] = avg - meanOfError;
+	result[1] = avg + meanOfError;
+	
+	return result;
+    }
+
+    public static double[] confidenceInterval2DArray(double[][] x, double zScore, int columnOrder) {
+	double avg = average2DArray(x, columnOrder);
+	double std = standardDiv2DArray(x, columnOrder);
+	double meanOfError = zScore * std / Math.sqrt(x.length);
+	double[] result = new double[2];
+
+	result[0] = avg - meanOfError;
+	result[1] = avg + meanOfError;
+	
+	return result;
     }
 
     public static void print1DArray(double[] array) {
