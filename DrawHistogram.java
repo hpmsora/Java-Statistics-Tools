e4a1c16a15e40d9eb1d9e887ceedb9f93286fb5d
Won Yong Ha <hpmsora@gmail.com>
2017-10-04 15:32:29 -0400

    Edit Draw Histogram file

--- DrawHistogram.java
+++ DrawHistogram.java
@@ -5,61 +5,58 @@ import javax.swing.*;
 
 public class DrawHistogram extends JPanel {
 
-    private String _title = "";
-    private double[] _values;
-    private double _x_start, _x_end, _x_interval;
-    private int _y_start, _y_end, _y_interval;
+  private String _title = "";
+  private double[] _values;
+  private double _x_start, _x_end, _x_interval;
+  private int _y_start, _y_end, _y_interval;
 
-    public DrawHistogram(String title, double[] values, double x_start, double x_end, double x_interval, int y_start, int y_end, int y_interval) {
-	_title = title;
-	_values = values;
-	_x_start = x_start;
-	_x_end = x_end;
-	_x_interval = x_interval;
-	_y_start = y_start;
-	_y_end = y_end;
-	_y_interval = y_interval;
-    }
-    
-    public void paintComponent(Graphics g) {
-	int[] calcArray = calculateArray();
-	int calcArraySize = calcArray.length;
-	int graphInterval = (int) Math.ceil(800 / (double) calcArraySize);
-	
-	super.paintComponent(g);
-	this.setBackground(Color.WHITE);
+  public DrawHistogram(String title, double[] values, double x_start, double x_end, double x_interval, int y_start, int y_end, int y_interval) {
+		_title = title;
+		_values = values;
+		_x_start = x_start;
+		_x_end = x_end;
+		_x_interval = x_interval;
+		_y_start = y_start;
+		_y_end = y_end;
+		_y_interval = y_interval;
+  }
 
-	for (int i = 0; i < calcArraySize; i++) {
-	    g.setColor(Color.BLUE);
-	    g.fillRect(100 + graphInterval * i, 400 - calcArray[i] * 400 / ((_y_end - _y_start) / _y_interval), graphInterval, calcArray[i] * 400 / ((_y_end - _y_start) / _y_interval));
+  public void paintComponent(Graphics g) {
+		int[] calcArray = calculateArray();
+		int calcArraySize = calcArray.length;
+		int graphInterval = (int) Math.ceil(800 / (double) calcArraySize);
+		
+		super.paintComponent(g);
+		this.setBackground(Color.WHITE);
+
+		for (int i = 0; i < calcArraySize; i++) {
+			g.setColor(Color.BLACK);
+			g.fillRect(100 + graphInterval * i, 400 - calcArray[i] * 400 / ((_y_end - _y_start) / _y_interval), graphInterval, calcArray[i] * 400 / ((_y_end - _y_start) / _y_interval));
+		}
+		    
+		g.setColor(Color.BLACK);
+		g.drawString(_title, 50, 30);
 	}
-	    
-	g.setColor(Color.BLACK);
-	g.drawString(_title, 250, 50);
-	
-    }
 
-    public int[] calculateArray() {
-	int[] result = new int[(int) ((_x_end - _x_start) / _x_interval)];
-	double currentValue, currentX;
-	int order;
-	
-	for (int i = 0; i < _values.length; i++) {
+	public int[] calculateArray() {
+		int[] result = new int[(int) ((_x_end - _x_start) / _x_interval)];
+		double currentValue, currentX;
+		int order;
+		
+		for (int i = 0; i < _values.length; i++) {
 	    currentValue = _values[i];
 	    currentX = _x_start;
 	    order = 0;
-	    
-	    while (currentX < _x_end) {
-		if(currentValue >= currentX && currentValue < (currentX + _x_interval)) {
-		    result[order] += 1;
-		    break;
-		}
-		
-		currentX += _x_interval;
-		order++;
-	    }
-	}
 
-	return result;
-    }
+	    while (currentX < _x_end) {
+				if(currentValue >= currentX && currentValue < (currentX + _x_interval)) {
+					result[order] += 1;
+					break;
+				}
+				currentX += _x_interval;
+				order++;
+			}
+		}
+		return result;
+	}
 }
--- test.java
+++ test.java
@@ -2,14 +2,14 @@ import statisticstools.Methods;
 import java.lang.Math;
 
 public class test {
-    public static void main(String[] args) {
-	double[] x = new double[100];
-	
-	Methods a = new Methods();
+  public static void main(String[] args) {
+  	double[] x = new double[100];
+  	
+  	Methods a = new Methods();
 
-	x = a.generateRandom1DArray(100, 0, 10);
-	
-	a.print1DArray(x);
-	a.drawHistogram("Histogram 1", x, 0, 10, 1, 0, 20, 1);
-    }
+  	x = a.generateRandom1DArray(100, 0, 10);
+  	
+  	a.print1DArray(x);
+  	a.drawHistogram("Histogram 1", x, 0, 10, 1, 0, 20, 1);
+  }
 }
